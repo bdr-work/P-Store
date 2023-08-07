e908503009fb3fb19f3a624bf071183daa5d32d5
bdr-work <123708937+bdr-work@users.noreply.github.com>
2023-08-08 00:51:39 +0300

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const databaseConnection = () => {
-  mongoose.connect(process.env.DB_URI).then((conn) => {
-    console.log(`Database connection ${conn.connection.host}`);
-  });
-  //.catch((err)=>{
-  //  console.log(`Database Error ${err}`);
-  //  process.exit(1);
-  // })
+const databaseConnection = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 module.exports = databaseConnection;
